ad59cb2ee9ccaac806d23d76e1cbe5bc0ac99e42
André Almeida <fda.andre@gmail.com>
2018-04-12 22:38:14 -0300

    add mc658

---
+++ mc658/lab01/pmr/pmr_e_h.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * MC658 - Projeto e Análise de Algoritmos III - 1s2018
+ * Prof: Flavio Keidi Miyazawa
+ * PED: Francisco Jhonatas Melo da Silva
+ * Usa ideias e código de Mauro Mulati e Flávio Keidi Miyazawa 
+ ******************************************************************************/
+
+/*******************************************************************************
+ * EDITE ESTE ARQUIVO APENAS ONDE INDICADO
+ * DIGITE SEU RA: ______
+ * SUBMETA SOMENTE ESTE ARQUIVO
+ ******************************************************************************/
+
+#include <iostream>
+#include <float.h>
+#include "pmr_e_h.h"
+//typedef vector<vector<double> > matriz;
+
+int algE(int capacity, int quantItens, vector<int> s, vector<int> v, matriz &relation, vector<int>& itensMochila, int maxTime)
+/*******************************************************************************
+ * SUBSTITUIA O CONTEÚDO DESTE MÉTODO POR SUA IMPLEMENTAÇÃO EXATA.
+ * ENTRETANTO, NÃO ALTERE A ASSINATURA DO MÉTODO.
+ ******************************************************************************/
+{
+	itensMochila[0] = 0;
+	itensMochila[1] = 1;
+	return 22;
+}
+
+int algH(int capacity, int quantItens, vector<int> s, vector<int> v, matriz &relation, vector<int>& itensMochila, int maxTime)
+/*******************************************************************************
+ * SUBSTITUIA O CONTEÚDO DESTE MÉTODO POR SUA IMPLEMENTAÇÃO HEURÍSTICA.
+ * ENTRETANTO, NÃO ALTERE A ASSINATURA DO MÉTODO.
+ ******************************************************************************/
+{
+	return 0;
+}
+
+
